88e271d87cee7faf7bb2b9f8c667f2b6ae4946b3
KindFest <kindfey@ya.ru>
2020-06-24 19:37:55 +0300

    Задание к первому уроку

---
+++ Les_1_Step_1.py
@@ -0,0 +1,9 @@
+var_one = 10
+var_two = "Количество попыток:"
+print(var_two,var_one)
+lovest_number = input("Введите любимое число: ")
+print("Это число как строка", lovest_number)
+lovest_number = int(lovest_number)
+print("Это любимое число как число", lovest_number)
+var_one += 2
+print(var_two,var_one)
